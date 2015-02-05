8a5f964794803bfd92d35ea330babdda2e24d743
Neilda <neilda@neildapacquing.com>
2015-02-05 01:36:13 +0000

    Made a has_and_belongs_to_many change in the database.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150201152920) do
+ActiveRecord::Schema.define(version: 20150204220719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(version: 20150201152920) do
 
   add_index "games", ["game_id"], name: "index_games_on_game_id", using: :btree
   add_index "games", ["user_id"], name: "index_games_on_user_id", using: :btree
+
+  create_table "games_users", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "pieces", force: true do |t|
     t.string   "name"
