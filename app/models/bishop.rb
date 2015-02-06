b24ee94a3cf636d9318497e7e8da1c6431c74f74
Samuel <samueljmartini@gmail.com>
2015-02-06 16:21:49 +0000

    a more RoR code

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,20 +1,3 @@
-class Bishop
-  attr_accessor :color
-  attr_accessor :type
-  attr_accessor :starting_pos
-  def initialize(color, type, starting_pos)
-    @color = color
-    @type = type
-    @starting_pos = starting_pos
-  end
-
-    #E.g. transform the instance <Rook:0xb983ad9c @color=:white, @type=:rook, @starting_pos=:a1> 
-    #in  :wRa1
-  def notation
-    color = @color.to_s
-    type = @type.to_s
-    starting_pos = @starting_pos.to_s
-    abbr = color[0] + type[0].capitalize + starting_pos
-    abbr.to_sym
-  end
+class Bishop < Piece
+ 
 end
--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -9,17 +9,28 @@ class GameTest < ActiveSupport::TestCase
 
     game = Game.create(:user_id => 1)
        
-    expected =  game.populate_board[0][0] = Rook.new(:white, :rook, [0,0]),
-                game.populate_board[0][1] = Knight.new(:white, :knight, [0,1]),
-                game.populate_board[0][2] = Bishop.new(:white, :bishop, [0,2]),
-                game.populate_board[0][3] = Queen.new(:white, :queen, [0,3]),
-                game.populate_board[0][4] = King.new(:white, :king, [0,4]),
-                game.populate_board[0][5] = Bishop.new(:white, :bishop, [0,5]),
-                game.populate_board[0][6] = Knight.new(:white, :knight, [0,6]),
-                game.populate_board[0][7] = Rook.new(:white, :rook, [0,7])
+    expected =  Rook.new(:color => :white, :name => :rook, :position => [0,0]),
+                Knight.new(:color => :white, :name => :knight, :position => [0,1]),
+                Bishop.new(:color => :white, :name => :bishop, :position => [0,2]),
+                Queen.new(:color => :white, :name => :queen, :position => [0,3]),
+                King.new(:color => :white, :name => :king, :position => [0,4]),
+                Bishop.new(:color => :white, :name => :bishop, :position => [0,5]),
+                Knight.new(:color => :white, :name => :knight, :position => [0,6]),
+                Rook.new(:color => :white, :name => :rook, :position =>[0,7])
+                # Pawn.new(:color => :white, :name => :pawn, :position => [1,0]),
+                # Pawn.new(:color => :white, :name => :pawn, :position => [1,1]),
+                # Pawn.new(:color => :white, :name => :pawn, :position => [1,2]),
+                # Pawn.new(:color => :white, :name => :pawn, :position => [1,3]),
+                # Pawn.new(:color => :white, :name => :pawn, :position => [1,4]),
+                # Pawn.new(:color => :white, :name => :pawn, :position => [1,5]),
+                # Pawn.new(:color => :white, :name => :pawn, :position => [1,6]),
+                # Pawn.new(:color => :white, :name => :pawn, :position => [1,7])
+                
       
-    actual = game.populate_board[0].each {|x| puts x }
-    
+    actual =   game.populate_board[0]
+                
+              
+
     assert_equal expected, actual
   end
 end
