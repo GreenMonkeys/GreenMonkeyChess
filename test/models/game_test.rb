c9501a25f7948cfb7e9d39b70e8921020ce47235
Denis Martens <dnismartens@gmail.com>
2015-02-13 00:58:28 +0000

    adjust tests to check movements in both directions

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -49,7 +49,7 @@ class GameTest < ActiveSupport::TestCase
     game.rooks.create!(:y_axis => 5, :x_axis => 4)
 
     # move Pawn with the very first move of a game
-    test = game.is_obstructed([3,3], [5,5])
+    test = game.is_obstructed([5,5], [3,3])
 
     assert_not test
   end
@@ -57,6 +57,7 @@ class GameTest < ActiveSupport::TestCase
   test "proposed horizontal move is obstructed" do
     game = Game.create(:user_id => 1)
     game.rooks.create!(:y_axis => 3, :x_axis => 4)
+    game.rooks.create!(:y_axis => 4, :x_axis => 4)
 
     # move Pawn with the very first move of a game
     test = game.is_obstructed([3,3], [3,5])
@@ -66,10 +67,11 @@ class GameTest < ActiveSupport::TestCase
 
   test "proposed horizontal move is not obstructed" do
     game = Game.create(:user_id => 1)
+    game.rooks.create!(:y_axis => 3, :x_axis => 4)
     game.rooks.create!(:y_axis => 4, :x_axis => 4)
 
     # move Pawn with the very first move of a game
-    test = game.is_obstructed([3,3], [3,5])
+    test = game.is_obstructed([3,5], [3,3])
 
     assert_not test
   end
@@ -79,7 +81,7 @@ class GameTest < ActiveSupport::TestCase
     game.rooks.create!(:y_axis => 4, :x_axis => 3)
 
     # move Pawn with the very first move of a game
-    test = game.is_obstructed([3,3], [5,3])
+    test = game.is_obstructed([3,3], [6,3])
 
     assert test
   end
@@ -89,7 +91,7 @@ class GameTest < ActiveSupport::TestCase
     game.rooks.create!(:y_axis => 4, :x_axis => 4)
 
     # move Pawn with the very first move of a game
-    test = game.is_obstructed([3,3], [5,3])
+    test = game.is_obstructed([6,3], [3,3])
 
     assert_not test
   end
