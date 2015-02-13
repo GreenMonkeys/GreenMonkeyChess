31b19cf0ba6f9f330660c7476890e4964f023c3c
Neilda <neilda@neildapacquing.com>
2015-02-13 00:54:29 +0000

    remove devise from game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,5 @@
 class Game < ActiveRecord::Base
-	has_many :users
-	has_many :pieces
+	has_and_belongs_to_many :users
+ 	has_many :pieces
 
-	devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 end
