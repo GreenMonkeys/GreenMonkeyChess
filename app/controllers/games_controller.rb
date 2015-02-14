0544a295fdc24fc3f61cd4ab1c9e144e223cefe9
WeiHsi <weihsi.hu@gmail.com>
2015-02-14 15:06:38 +0000

    fixed indentation in Games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,12 @@
 class GamesController < ApplicationController
 
 	def show
-	@game = Game.find(params[:id])
-	
-	# used to initialize board
-    # @board = @game.populate_board
+		@game = Game.find(params[:id])
+		
+		# used to initialize board
+	    # @board = @game.populate_board
 
-    @board = @game.board
+	    @board = @game.board
 	end
 
 end
