524226af9a6f525602a0c639776e985ab9f886a2
WeiHsi <weihsi.hu@gmail.com>
2015-02-21 16:35:55 +0000

    migrate remove :game_id from :games

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150207172321) do
+ActiveRecord::Schema.define(version: 20150221163159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "games", force: true do |t|
-    t.integer  "game_id"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "games", ["game_id"], name: "index_games_on_game_id", using: :btree
   add_index "games", ["user_id"], name: "index_games_on_user_id", using: :btree
 
   create_table "games_users", force: true do |t|
