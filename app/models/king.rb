f7b00f0e647e2ea722f9600a3879c5719b17b81b
WeiHsi <weihsi.hu@gmail.com>
2015-03-13 08:47:55 +0000

    check logic

--- app/models/king.rb
+++ app/models/king.rb
@@ -6,4 +6,82 @@ class King < Piece
 			return false
 		end
 	end
+
+	def checked?(position)
+		case self.color
+		when "white"
+			self.game.pieces.where(:color => "black").find_each do |piece|
+				if piece.valid_move?(position)
+					return true
+				end
+			end
+		when "black"
+			self.game.pieces.where(:color => "white").find_each do |piece|
+				if piece.valid_move?(position)
+					return true
+				end
+			end
+		end
+	end
+
+	def check_mate?
+		self.possible_moves.each do |move|
+			if self.valid_move?(move)
+				if self.checked?(move)
+					self.check_piece.game.find_path(self.check_piece.current_position, self.current_position).each do |step|
+						case self.color
+						when "white"
+							self.game.pieces.where(:color => "white").find_each do |piece|
+								if piece.valid_move?(move)
+										if self.check_piece.game.is_obstructed?(step)
+										return true
+										end
+								end
+							end
+						when "black"
+							self.game.pieces.where(:color => "black").find_each do |piece|
+								if piece.valid_move?(move)
+										if self.check_piece.game.is_obstructed?(step)
+										return true
+										end
+								end
+							end
+						end
+					end
+				end
+			end
+		end
+	end
+
+	def possible_moves
+		moves = []
+		moves << [self.y_axis, self.x_axis + 1] 
+		moves << [self.y_axis, self.x_axis - 1]
+		moves << [self.y_axis + 1, self.x_axis]
+		moves << [self.y_axis - 1, self.x_axis]
+		moves << [self.y_axis + 1, self.x_axis + 1]
+		moves << [self.y_axis + 1, self.x_axis - 1]
+		moves << [self.y_axis - 1, self.x_axis + 1]
+		moves << [self.y_axis - 1, self.x_axis - 1]
+		moves
+	end
+
+	def check_piece
+		if self.checked?(self.current_position)
+			case self.color
+			when "white"
+				self.game.pieces.where(:color => "black").find_each do |piece|
+					if piece.valid_move?(self.current_position)
+						piece
+					end
+				end
+			when "black"
+				self.game.pieces.where(:color => "white").find_each do |piece|
+					if piece.valid_move?(self.current_position)
+						piece
+					end
+				end
+			end
+		end
+	end
 end
